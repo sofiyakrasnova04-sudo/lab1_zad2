3a6f0ddac31acc0b4d778c56e0cc86be3264d864
Sofiya Krasnova <sofiya.krasnova04@gmail.com>
2025-10-12 13:43:02 +0300

    zad1

---
+++ lab2/zad1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import Int32
+
+def even_numbers_publisher():
+    # Инициализация узла
+    rospy.init_node('even_numbers_publisher', anonymous=True)
+    
+    # Создание публикатора
+    pub = rospy.Publisher('even_numbers', Int32, queue_size=10)
+    
+    # Частота 10 Гц
+    rate = rospy.Rate(10)
+    
+    # Счетчик четных чисел
+    even_number = 0
+    
+    rospy.loginfo("Even numbers publisher started. Publishing at 10 Hz...")
+    
+    while not rospy.is_shutdown():
+        # Публикация четного числа
+        pub.publish(even_number)
+        rospy.loginfo("Published: %d", even_number)
+        
+        # Увеличение на 2 для следующего четного числа
+        even_number += 2
+        
+        # Ожидание для поддержания частоты 10 Гц
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        even_numbers_publisher()
+    except rospy.ROSInterruptException:
+        rospy.logerr("ROS interrupt exception")
+    except KeyboardInterrupt:
+        rospy.loginfo("Node stopped by user")
